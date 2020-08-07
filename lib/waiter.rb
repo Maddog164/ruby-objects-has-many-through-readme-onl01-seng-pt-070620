5b1de54ba0b0b3b3038628bdd395195aaf5dd53b
Denise Henderson <denise@henderson3.us>
2020-08-07 22:35:51 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
 
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+
+  def initialize(name, yrs_experence)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
 end
